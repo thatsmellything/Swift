5342cb35cc819b48509417d8f341b23eae964f50
Jensen Judkins <thesmelliestman@gmail.com>
2018-09-24 09:45:07 -0600

    crreated new playground

--- TrialblahMonday.playground/Contents.swift
+++ TrialblahMonday.playground/Contents.swift
@@ -2,4 +2,3 @@
 
 import UIKit
 
-var str = "Hello, playground"
---
+++ maybethiswilwork.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var Jensen = "The best human cause just duh"
+Jensen
+
+var x = 0
+var y = 1
+while (x<y)
+{
+    x+1
+    y+1
+    
+    
+}
